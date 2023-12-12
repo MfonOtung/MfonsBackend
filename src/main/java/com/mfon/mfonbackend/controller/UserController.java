33a2897f7091604c0a04565a5c35bd28416c893f
MfonOtung <mfonotung@gmail.com>
2023-12-12 00:06:28 +0000

    User Class with Getter and Setter

--- src/main/java/com/mfon/mfonbackend/controller/UserController.java
+++ src/main/java/com/mfon/mfonbackend/controller/UserController.java
@@ -12,28 +12,29 @@ public class UserController {
         return "this is a test";
     }
 
+    // TODO: create a method called getUsers and shall return a string array
     public String[] getUsers(){
         return new String[]{"Users"};
     }
 
-    // TODO: cre
-
-
     //TODO: create a method called createUSer and shall post a string
     @PostMapping("/create-a-user")
-    public String createUser (){
+    public String createUser(){
+
         return "Input username";
     }
 
     // TODO: create a method called updateUSer and shall return an int
-    @PutMapping("input-update")
-    public int updateUser (){
+    @PutMapping("/input-update")
+    public int updateUser(){
+
         return 0;
     }
 
     // TODO: create a method called deleteUSer and shall return a boolean
-    @DeleteMapping("delete-user")
+    @DeleteMapping("/delete-user")
     public Boolean deleteUser() {
+
         return null;
     }
 }
---
+++ src/main/java/com/mfon/mfonbackend/model/User.java
@@ -0,0 +1,71 @@
+package com.mfon.mfonbackend.model;
+
+import java.util.Date;
+
+public class User {
+
+    public User(){
+    }
+    public String name;
+
+    public String password;
+
+    public String username;
+
+    public Date birthdate;
+
+    public String phone;
+
+    public String address;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+
